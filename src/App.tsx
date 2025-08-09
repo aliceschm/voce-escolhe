ef46df48f2e5d885d9bb56134445684b99a33340
Alice <alicesch@ymail.com>
2025-08-09 13:40:34 -0300

    test: adding card component for visual tests

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import "./App.css";
-import Button from "./components/Button";
+import "./styles/themes.css";
+import Card from "./components/Card";
+import { useCardState } from "./hooks/useCardState";
 
 function App() {
-  function handleClick() {
-    alert("Clicou no botão!");
-  }
-
-  return <Button text="Test" onClick={handleClick} />;
+  const { name, setName, options, updateOption } = useCardState();
+  return (
+    <Card
+      name={name}
+      options={options}
+      onChangeName={setName}
+      onChangeOption={updateOption}
+      nameLabel="Nome"
+      namePlaceholder="Digite seu nome"
+      optionLabel={(i) => `Opção ${i + 1}`}
+      optionPlaceholder={(i) => `Digite a opção ${i + 1}`}
+    />
+  );
 }
 
 export default App;
