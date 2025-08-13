d532767591f298da52d9e2c66bb0b009c01d1b68
Alice <alicesch@ymail.com>
2025-08-13 12:02:22 -0300

    refactor: rendring only the main component Game

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,5 @@
-import "./styles/themes.css";
-import Card from "./components/Card";
-import { useCardState } from "./hooks/useCardState";
+import { Game } from "./pages/Game";
 
-function App() {
-  const { name, setName, options, updateOption } = useCardState();
-  return (
-    <Card
-      name={name}
-      options={options}
-      onChangeName={setName}
-      onChangeOption={updateOption}
-      nameLabel="Nome"
-      namePlaceholder="Digite seu nome"
-      optionLabel={(i) => `Opção ${i + 1}`}
-      optionPlaceholder={(i) => `Digite a opção ${i + 1}`}
-    />
-  );
+export default function App() {
+  return <Game />;
 }
-
-export default App;
