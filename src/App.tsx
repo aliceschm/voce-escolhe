61dd33bb43dc4c279b2e9cac5c9834331a4663eb
Alice <alicesch@ymail.com>
2025-08-16 10:49:55 -0300

    chore: comments for ideas and to do's

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { Header } from "./components/Header";
 import { Game } from "./pages/Game";
+//TODO:
+//  opcao de cancelar participante. ex escolheu 5 mas no meio do jogo so 4 vao participar
+// validacao de inputs, pelo menos uma opcao tem que estar preenchida
 
 export default function App() {
   return (
