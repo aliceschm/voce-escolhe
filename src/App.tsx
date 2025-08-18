8de9da72bab94cba6e14c988e800469c3b40918d
Alice <alicesch@ymail.com>
2025-08-17 21:56:45 -0300

    style: adding react-bits Galaxy component as background

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,39 @@
 import { Header } from "./components/Header";
 import { Game } from "./pages/Game";
-//TODO:
-//  opcao de cancelar participante. ex escolheu 5 mas no meio do jogo so 4 vao participar
-// validacao de inputs, pelo menos uma opcao tem que estar preenchida
+import Galaxy from "./components/Galaxy";
 
 export default function App() {
   return (
-    <>
-      <Header />
-      <main>
-        <Game />
-      </main>
-    </>
+    <div style={{ position: "relative", minHeight: "100vh" }}>
+      {/* Fundo Galaxy fixo */}
+      <div
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: "100vw",
+          height: "100vh",
+          zIndex: 0,
+          overflow: "hidden",
+        }}
+      >
+        <Galaxy
+          mouseRepulsion={true}
+          mouseInteraction={true}
+          density={1.5}
+          glowIntensity={0.5}
+          saturation={0.8}
+          hueShift={240}
+        />
+      </div>
+
+      {/* Conteúdo do site acima do fundo */}
+      <div style={{ position: "relative", zIndex: 1 }}>
+        <Header />
+        <main>
+          <Game />
+        </main>
+      </div>
+    </div>
   );
 }
